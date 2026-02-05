1e52c4b51c0fce2b45b5a21d599042ab7eec5ad2
fahdaguenouz <faguenouz@gmail.com>
2026-02-05 12:26:02 +0100

    changing the facebook url

--- internal/services/username/networks.go
+++ internal/services/username/networks.go
@@ -7,7 +7,7 @@ type Network struct {
 
 //  networks 
 var DefaultNetworks = []Network{
-	{Name: "facebook", URL: func(h string) string { return "https://www.facebook.com/" + h }},
+	{Name: "facebook", URL: func(h string) string { return "https://web.facebook.com/" + h }},
 	{Name: "twitter", URL: func(h string) string { return "https://x.com/" + h }},
 	{Name: "instagram", URL: func(h string) string { return "https://www.instagram.com/" + h + "/" }},
 	{Name: "tiktok", URL: func(h string) string { return "https://www.tiktok.com/@" + h }},
